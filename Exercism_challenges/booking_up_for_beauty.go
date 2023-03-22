f6e39deca7db01c6a93d5fe54f8e1e8ff8b0cb23
Sadat Issah <tommyleespartan0@gmail.com>
2023-03-22 12:29:43 +0000

    booking-up-for-beauty challenge solved

---
+++ Exercism_challenges/booking_up_for_beauty.go
@@ -0,0 +1,48 @@
+package booking
+
+import "time"
+
+// Schedule returns a time.Time from a string containing a date.
+func Schedule(date string) time.Time {
+    layout := "1/2/2006 15:04:05"
+    time,err:= time.Parse(layout,date)
+    
+    if err != nil{
+       panic("Unknown date format: " + date)
+    }
+
+     return time
+}
+
+// HasPassed returns whether a date has passed.
+func HasPassed(date string) bool {
+	    layout := "January 2, 2006 15:04:05"
+    
+        parse_date,_ := time.Parse(layout,date)
+        now := time.Now()
+    
+        return parse_date.Before(now)
+}
+
+// IsAfternoonAppointment returns whether a time is in the afternoon.
+func IsAfternoonAppointment(date string) bool {
+		layout := "Monday, January 2, 2006 15:04:05"
+    
+        parse_date,_ := time.Parse(layout,date)
+    
+      	hour := parse_date.Hour()
+	   return hour >= 12 && hour < 18
+}
+
+// Description returns a formatted string of the appointment time.
+func Description(date string) string {
+    t := Schedule(date)
+    layout := "You have an appointment on Monday, January 2, 2006, at 15:04."
+	return t.Format(layout)
+}
+
+// AnniversaryDate returns a Time with this year's anniversary.
+func AnniversaryDate() time.Time {
+    
+    return time.Date(time.Now().Year(), time.September, 15, 0, 0, 0, 0, time.UTC)
+}
